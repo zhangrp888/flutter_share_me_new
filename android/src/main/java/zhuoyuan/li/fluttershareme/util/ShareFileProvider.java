f3f230fad40363b680f42f29214d9a6266383102
zhangrp <shyn@192.168.10.39>
2023-11-19 18:47:39 +0800

    1
    1

---
+++ android/src/main/java/zhuoyuan/li/fluttershareme/util/ShareFileProvider.java
@@ -0,0 +1,6 @@
+package zhuoyuan.li.fluttershareme.util;
+
+import androidx.core.content.FileProvider;
+
+public class ShareFileProvider extends FileProvider {
+}
---
+++ example/android/app/src/main/java/zhuoyuan/li/fluttersharemeexample/MainActivity.java
@@ -0,0 +1,7 @@
+package zhuoyuan.li.fluttersharemeexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
